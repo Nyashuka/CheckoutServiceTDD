5abf7f9c4c8b45262f9bda34e7886900d7b677df
Nyashuka <godnebeles@gmail.com>
2023-12-20 18:17:47 +0200

    Implement offer by category

---
+++ CheckoutServiceCore/Offers/FactorByCategoryOffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutServiceCore.Offers
+{
+    public class FactorByCategoryOffer : Offer
+    {
+        public readonly Category category;
+        public readonly int factor;
+
+        public FactorByCategoryOffer(Category category, int factor)
+        {
+            this.category = category;
+            this.factor = factor;
+        }
+
+        public override int CalculatePoints(ICheck check)
+        {
+            int points = check.GetCostByCategory(category);
+            return points * (factor - 1);
+        }
+
+        public override bool CheckCondition(ICheck check)
+        {
+            return check.GetCostByCategory(category) > 0;
+        }
+    }
+}
